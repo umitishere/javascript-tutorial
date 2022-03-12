e5887d6e3da14f14bdd3bd603f2733b18085d21f
umitishere <umitsancakli98@gmail.com>
2022-03-12 15:31:01 +0300

    Learn usage of constructor, super and extends keywords

--- js/code.js
+++ js/code.js
@@ -1,9 +1,8 @@
 // JavaScript Classes
 class Pizza {
 
-    constructor(pizzaType, pizzaSize) {
+    constructor(pizzaSize) {
 
-        this.type = pizzaType;
         this.size = pizzaSize;
         this.crust = "original";
         this.toppings = [];
@@ -18,29 +17,25 @@ class Pizza {
         this.crust = pizzaCrust;
     }
 
-    getToppings() {
-        return this.toppings;
+}
+
+class SpecialtyPizza extends Pizza {
+
+    constructor(pizzaType) {
+
+        // Calling constructor of parent class with super keyword.
+        // We should call super before we use "this" keyword.
+        super(pizzaType);
+
+        this.type = "The Works";
+
     }
 
-    setToppings(topping) {
-        this.toppings.push(topping); // Add new item to toppings array.
-    }
-
-    bake() {
-        console.log(
-            `Baking a ${this.size} ${this.type} ${this.crust} crust pizza.`
-        );
+    slice() {
+        console.log(`Our ${this.type} ${this.size} pizza has 8 slices.`);
     }
 
 }
 
-const myPizza = new Pizza("pepperoni", "small");
-
-myPizza.setCrust("thin");
-
-myPizza.setToppings("sausage");
-myPizza.setToppings("olives");
-
-myPizza.bake();
-
-console.log(myPizza.getToppings());
+const mySpecialty = new SpecialtyPizza("medium");
+mySpecialty.slice();
